7262fc5d12f6dc4f11dee9efb978a49079e9b64c
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2021-02-01 21:46:11 -0500

    updated the package

--- inst/doc/Correlation.R
+++ inst/doc/Correlation.R
@@ -1,19 +1,19 @@
-## ---- include = FALSE-----------------------------------------------------------
+## ---- include = FALSE-----------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- echo = FALSE, out.width="75%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="75%", fig.align='center'----
 knitr::include_graphics("pictures/correl/1.png")
 
-## ---- echo = FALSE, out.width="75%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="75%", fig.align='center'----
 knitr::include_graphics("pictures/correl/2.png")
 
-## ---- echo = FALSE, out.width="75%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="75%", fig.align='center'----
 knitr::include_graphics("pictures/correl/3.png")
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 library(rio)
 library(ggplot2)
 cleanup <- theme(panel.grid.major = element_blank(), 
@@ -26,7 +26,7 @@ cleanup <- theme(panel.grid.major = element_blank(),
 exam <- import("data/exam_data.csv")
 liar <- import("data/liar_data.csv")
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 #from chapter 5 notes
 scatter <- ggplot(exam, aes(Anxiety, Exam))
 scatter +
@@ -35,7 +35,7 @@ scatter +
   ylab("Exam Score") +
   cleanup
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 #from chapter 5 notes + coord_cartesian
 scatter <- ggplot(exam, aes(Anxiety, Exam))
 scatter +
@@ -46,18 +46,18 @@ scatter +
   coord_cartesian(xlim = c(50,100), ylim = c(0,100))
   #just example numbers, you would want to use the real scale of the data
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 var(exam$Revise)
 var(exam$Exam)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 cov(exam$Revise, exam$Exam)
 plot(exam$Revise, exam$Exam)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 cor(exam$Revise, exam$Exam)
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 cor(exam[ , -1], 
     use="pairwise.complete.obs", 
     method = "pearson")
@@ -66,31 +66,31 @@ cor(exam[ , -1],
     use="pairwise.complete.obs", 
     method = "kendall")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 library(Hmisc)
 rcorr(as.matrix(exam[ , -1]), type = "pearson")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 cor.test(exam$Revise,
          exam$Exam,
          method = "pearson")
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 str(liar)
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 with(liar, cor.test(Creativity, Position, method = "spearman"))
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 with(liar, cor.test(Creativity, Position, method = "kendall"))
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 liar$Novice2 <- as.numeric(as.factor(liar$Novice))
 str(liar) #we had to factor because of the character variable
 with(liar, cor.test(Creativity, Novice2))
 plot(liar$Creativity, liar$Novice2)
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 library(cocor)
 new <- subset(liar, Novice == "First Time")
 old <- subset(liar, Novice == "Had entered Competition Before")
@@ -98,17 +98,17 @@ ind_data <- list(new, old)
 cocor(~Creativity + Position | Creativity + Position,
       data = ind_data)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 cocor(~Revise + Exam | Revise + Anxiety, 
       data = exam)
 
-## ---- echo = FALSE, out.width="75%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="75%", fig.align='center'----
 knitr::include_graphics("pictures/correl/9.png")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 library(ppcor)
 pcor(exam[ , -c(1)], method = "pearson")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 spcor(exam[ , -c(1)], method = "pearson")
 
--- inst/doc/Data-Visualization.R
+++ inst/doc/Data-Visualization.R
@@ -1,10 +1,10 @@
-## ---- include = FALSE-----------------------------------------------------------
+## ---- include = FALSE-----------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----eval = F-------------------------------------------------------------------
+## ----eval = F-------------------------------------------
 #  textline +
 #    stat_summary(fun.y = mean,
 #                 geom = "point") +
@@ -22,59 +22,59 @@ knitr::opts_chunk$set(
 #                       values = c("Black", "Grey")) +
 #    scale_x_discrete(labels = c("Baseline", "Six Months"))
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 library(rio)
 chickflick <- import("data/ChickFlick.sav")
 str(chickflick)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 table(chickflick$gender)
 
 table(chickflick$film)
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 chickflick$gender <- factor(chickflick$gender, #the variable you want to factor
                             levels = c(1,2), #the information already in the data
                             labels = c("Male", "Female")) #the labels for those levels
 
 table(chickflick$gender)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 library(reshape) #note: you could also use pivot_longer in tidyverse
 cricket <- import("data/Jiminy_Cricket.csv")
 head(cricket)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 longcricket <- melt(cricket,  #name of dataset
                     id = c("ID", "Strategy"), 
                     measured = c("Success_Pre", "Success_Post"))
 #you can actually leave measured blank
 head(longcricket)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 colnames(longcricket)[3:4] #just to figure out which ones
 colnames(longcricket)[3:4] <- c("Time", "Score")
 
-## ---- echo = FALSE, out.width="35%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="35%", fig.align='center'----
 knitr::include_graphics("pictures/graphs/badgraph.png")
 
-## ---- echo = FALSE, out.width="35%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="35%", fig.align='center'----
 knitr::include_graphics("pictures/graphs/bettergraph.png")
 
-## ---- echo = FALSE, out.width="50%", fig.align='center'-------------------------
+## ---- echo = FALSE, out.width="50%", fig.align='center'----
 knitr::include_graphics("pictures/graphs/deception.png")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 library(ggplot2)
 
-## ----eval = F-------------------------------------------------------------------
+## ----eval = F-------------------------------------------
 #  #an example
 #  myGraph <- ggplot(dataset,
 #                    aes(x_axis, y_axis,
 #                        color = legend_var,
 #                        fill = legend_var))
 
-## ----eval = F-------------------------------------------------------------------
+## ----eval = F-------------------------------------------
 #  #an example part 2
 #  myGraph +
 #    geom_bar() +
@@ -82,35 +82,35 @@ library(ggplot2)
 #    xlab("X Axis Label") +
 #    ylab("Y Axis Label")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 crickethist <- ggplot(data = cricket, #dataset
                       aes(x = Success_Pre) #only define X axis 
                       )
 crickethist
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 crickethist + 
   geom_histogram()
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 crickethist + 
   geom_histogram(binwidth = 1)
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 crickethist + 
   geom_histogram(binwidth = 1, color = 'purple', fill = 'magenta')
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 crickethist + 
   geom_histogram(binwidth = 1, color = 'purple', fill = 'magenta') + 
   xlab("Success Pre Test") + 
   ylab("Frequency")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 festival <- import("data/festival.csv")
 str(festival)
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 festivalhist <- ggplot(data = festival, aes(x = day1)) 
 festivalhist + 
   geom_histogram(binwidth = 1, color = 'blue') + 
@@ -118,7 +118,7 @@ festivalhist +
   ylab("Frequency") +
   theme_bw() #theme_classic() also good!
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 cleanup <- theme(panel.grid.major = element_blank(), #no grid lines
                 panel.grid.minor = element_blank(), #no grid lines
                 panel.background = element_blank(), #no background
@@ -127,25 +127,25 @@ cleanup <- theme(panel.grid.major = element_blank(), #no grid lines
                 legend.key = element_rect(fill = 'white'), #no legend background
                 text = element_text(size = 15)) #bigger text size
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 festivalhist + 
   geom_histogram(binwidth = 1, color = 'blue') + 
   xlab("Day 1 of Festival Hygiene") +
   ylab("Frequency") +
   cleanup
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 exam <- import("data/Exam_Anxiety.csv")
 str(exam)
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 table(exam$Gender)
 exam$Gender <- factor(exam$Gender,
                      levels = c(1,2),
                      labels = c("Male", "Female"))
 table(exam$Gender)
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 scatter <- ggplot(exam, aes(Anxiety, Exam))
 scatter +
   geom_point() +
@@ -153,14 +153,14 @@ scatter +
   ylab("Exam Score") +
   cleanup
 
-## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE--------
+## ----echo=TRUE,  fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 scatter + geom_point()+
   geom_smooth(method = 'lm', color = 'black', fill = 'blue') +
   xlab('Anxiety Score')+
   ylab('Exam Score')+
   cleanup
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 scatter2 <- ggplot(exam, aes(Anxiety, Exam, 
                              color = Gender, fill = Gender)) #why both?
 scatter2 +
@@ -177,18 +177,18 @@ scatter2 +
                      values = c("purple", "grey10"))
 
 
-## -------------------------------------------------------------------------------
+## -------------------------------------------------------
 library(GGally)
 ggpairs(data = exam[ , -1], #no participant variable
         title = "Exam Anxiety, Scores, and Gender")
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 str(chickflick) #already fixed gender
 chickflick$film <- factor(chickflick$film,
                     levels = c(1,2),
                     labels = c("Bridget Jones", "Memento"))
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 chickbar <- ggplot(chickflick, aes(film, arousal))
 chickbar + 
   stat_summary(fun = mean,
@@ -197,7 +197,7 @@ chickbar +
                color = "Black") +
   cleanup
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 chickbar + 
   stat_summary(fun = mean,
                geom = "bar",
@@ -209,7 +209,7 @@ chickbar +
                width = 0.2) +
   cleanup
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 chickbar + 
   stat_summary(fun.y = mean,
                geom = "bar",
@@ -225,7 +225,7 @@ chickbar +
   scale_x_discrete(labels = c("Girl Film", "Guy Film"))
 
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 chickbar2 <- ggplot(chickflick, aes(film, arousal, fill = gender))
 chickbar2 +
   stat_summary(fun.y = mean,
@@ -242,17 +242,17 @@ chickbar2 +
                     labels = c("Boys", "Girls"),
                     values = c("Gray30", "Gray"))
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 hiccups <- import("data/Hiccups.csv")
 str(hiccups)
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 longhiccups <- melt(hiccups, 
                     measured = c("Baseline", "Tongue", "Carotid", "Other"))
 str(longhiccups)
 colnames(longhiccups) <- c("Intervention", "Hiccups")
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 hiccupline <- ggplot(longhiccups, aes(Intervention, Hiccups))
 hiccupline +
   stat_summary(fun = mean, ##adds the points
@@ -267,11 +267,11 @@ hiccupline +
   ylab("Number of Hiccups") + 
   cleanup
 
-## ----echo=TRUE, message=FALSE, warning=FALSE------------------------------------
+## ----echo=TRUE, message=FALSE, warning=FALSE------------
 texting <- import("data/Texting.xlsx")
 str(texting)
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 texting$Group <- factor(texting$Group,
                        levels = c(1,2),
                        labels = c("Texting Allowed", "No Texting Allowed"))
@@ -281,7 +281,7 @@ longtexting <- melt(texting,
 str(longtexting)
 colnames(longtexting) <- c("Group", "Time", "Grammar_Score")
 
-## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE---------
+## ----echo=TRUE, fig.height=4, fig.width=8, message=FALSE, warning=FALSE----
 textline <- ggplot(longtexting, aes(Time, Grammar_Score, color = Group))
 textline +
   stat_summary(fun = mean,
